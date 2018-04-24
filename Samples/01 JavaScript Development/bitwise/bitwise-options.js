a658b958b41e031fc4cf61b8008679f0878cfaa4
Ori Calvo <ori.calvo@gmail.com>
2018-04-24 14:42:38 +0300

    bitwise solution

--- Samples/01 JavaScript Development/bitwise/bitwise-options.js
+++ Samples/01 JavaScript Development/bitwise/bitwise-options.js
@@ -10,14 +10,23 @@ var pita = 0;
 
 buildOptions();
 console.log(bitwiseOptions);
-/*
-var bitwiseOptions = {
-    'hummus': 1,
-    'hamutzim': 2,
-    'salat': 4,
-    'chips': 8
-};
-*/
+
+// pita => 9
+//pita => 0
+pita=set(pita, 'hummus');
+console.log("set(pita, 'hummus') => ",pita);
+console.log('pita => ',pita);
+console.log("is(pita, 'hummus') => ",is(pita, 'hummus'));
+
+// pita => 1
+pita=set(pita, 'chips');
+console.log("set(pita, 'chips') => ",pita);
+console.log('pita => ',pita);
+console.log("is(pita, 'chips') => ",is(pita, 'chips'));
+console.log("is(pita, 'salat') => ",is(pita, 'salat'));
+
+pita = unset(pita, 'chips');
+console.log("unset(pita, 'chips'); pita =>", pita);
 
 function buildOptions() {
     for (var i=0; i<options.length; i++){
@@ -37,9 +46,4 @@ function is(obj, option) {
 function set() {
 
 }
-
-// build options
-//
-
-//
 
