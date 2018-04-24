5a8b961d4b5a15a849672b7a27cbe36cfa70b1af
Ori Calvo <ori.calvo@gmail.com>
2018-04-24 11:09:34 +0300

    bitwise

--- Samples/01 JavaScript Development/bitwise/bitwise-options.js
+++ Samples/01 JavaScript Development/bitwise/bitwise-options.js
@@ -5,16 +5,36 @@ var options = [
     'chips' //8
 ];
 
+var bitwiseOptions = {};
+var pita = 0;
+
+buildOptions();
+console.log(bitwiseOptions);
+/*
 var bitwiseOptions = {
     'hummus': 1,
-    'hamutzim': 2
+    'hamutzim': 2,
+    'salat': 4,
+    'chips': 8
 };
-
-
-var shai = 15 // 1111
-
+*/
 
 function buildOptions() {
+    for (var i=0; i<options.length; i++){
+        bitwiseOptions[options[i]] = 1 << i;
+    }
+}
+
+
+is(pita, 'hummus') // false
+is(pita, 'hummus') // true
+
+
+function is(obj, option) {
+
+}
+
+function set() {
 
 }
 
