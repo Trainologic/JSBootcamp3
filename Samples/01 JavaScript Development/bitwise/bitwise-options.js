525d6d9fcc00039d6488c7fb9f9382a4a9b98869
Ori Calvo <ori.calvo@gmail.com>
2018-04-24 10:42:59 +0300

    bitwise theory

---
+++ Samples/01 JavaScript Development/bitwise/bitwise-options.js
@@ -0,0 +1,25 @@
+var options = [
+    'hummus', //1
+    'hamutzim', //2
+    'salat', //4
+    'chips' //8
+];
+
+var bitwiseOptions = {
+    'hummus': 1,
+    'hamutzim': 2
+};
+
+
+var shai = 15 // 1111
+
+
+function buildOptions() {
+
+}
+
+// build options
+//
+
+//
+
