11ec4591cdee6f03a58b825b62f9012801cc3279
Ori Calvo <ori.calvo@gmail.com>
2018-04-24 13:07:41 +0300

    inline condition

---
+++ Samples/01 JavaScript Development/inline condition/example.js
@@ -0,0 +1,24 @@
+var efes = NaN; // 0001
+if(efes==false) {
+    console.log('true');
+}
+
+
+var age =30;
+var res =(age>30)?trueFn(): (a=3);
+function c() {
+    return (a=3)
+}
+
+console.log((true)?(a=3):(a=2));
+var res = condition? {statment1}:{statment2};
+
+function trueFn() {
+    return 1;
+}
+if(age>30){
+    trueFn();
+}
+else{
+    falseFn();
+}
