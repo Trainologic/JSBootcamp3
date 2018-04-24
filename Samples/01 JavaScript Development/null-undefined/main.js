ded5ac201d6f15aeeccf319b058d82e5d087e765
Ori Calvo <ori.calvo@gmail.com>
2018-04-24 09:49:06 +0300

    null undefined

---
+++ Samples/01 JavaScript Development/null-undefined/main.js
@@ -0,0 +1,17 @@
+
+var a = 0;
+console.log(a);
+console.log('a == false =>',a == false);
+console.log('a === false =>',a === false);
+console.log('!!a =>',!!a);
+
+// if(typeof b === 'undefined' && b !== null){}
+// if(!!b){}
+// b <= null
+// null => false
+// !null => true
+// !!null => false
+
+// a <= '0'
+// !a => false
+// !!a => true
