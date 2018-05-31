54751662a110de9a7e93dedb0a8b921b0d525bd8
Ori Calvo <ori.calvo@gmail.com>
2018-05-31 11:18:36 +0300

    currying with es6

--- samples/07 Advanced JavaScript/currying.js
+++ samples/07 Advanced JavaScript/currying.js
@@ -7,6 +7,14 @@ function calc(operation) {
     }
 }
 
+// es6
+calc = (operation) => (a, b) =>{
+    switch (operation) {
+        case 'plus':
+            return a + b;
+    }
+};
+
 let res = calc('plus')(2,3);
 let plus = calc('plus')
 plus(2, 3);
