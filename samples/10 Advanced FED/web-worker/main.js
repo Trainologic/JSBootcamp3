dec272b6fd0c9a8b63f0ad072c4fc11a25afb7c0
Ori Calvo <ori.calvo@gmail.com>
2018-06-17 16:18:34 +0300

    web worker with loading

--- samples/10 Advanced FED/web-worker/main.js
+++ samples/10 Advanced FED/web-worker/main.js
@@ -2,10 +2,16 @@ $(document).ready(() => {
 
     if(window.Worker) {
         $counter = $('.counter');
+        $loading = $('.loading');
         const worker = new Worker('worker.js');
 
         worker.onmessage= (e)=>{
-            $counter.text(e.data);
+            if(e.data.done) {
+                $counter.text(e.data.message);
+            }
+            else{
+                $loading.text('loading: '+e.data.message+'%');
+            }
         }
 
         worker.postMessage(0);
--- samples/10 Advanced FED/web-worker/worker.js
+++ samples/10 Advanced FED/web-worker/worker.js
@@ -1,5 +1,14 @@
 importScripts('helper.js');
-onmessage=()=>{
-    postMessage(calc());
+onmessage = () => {
+    let k = 0;
+    for (let j = 0; j < 1000; j++) {
+        for (let i = 0; i < 10000000; i++) {
+            k += j + i;
+        }
+        postMessage({done: false, message: Math.round(k/500000400000000)});
+
+    }
+
+    postMessage({done: true, message: k});
 }
 
